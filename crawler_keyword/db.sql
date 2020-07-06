6cf9e0818fa8d04a4819cce2baea636ce9e0905c
ha.th <ha.th@financialdeepmind.com>
2020-07-06 17:15:50 +0700

    dataset

--- crawler_keyword/db.sql
+++ crawler_keyword/db.sql
@@ -1,46 +1,39 @@
-create table image_urls
-(
-    id        bigint auto_increment
-        primary key,
-    url       varchar(255) null,
-    image_url varchar(255) null
-);
-
 create table industries
 (
-    code varchar(20)  not null
+    code varchar(20)               not null
         primary key,
-    name varchar(255) null
+    name varchar(255) charset utf8 null
 );
 
 create table keywords
 (
     id   int auto_increment
         primary key,
-    name varchar(100) null
+    name varchar(255) charset utf8 null
 );
 
 create table posts
 (
     id               bigint auto_increment
         primary key,
-    title            varchar(255)  null,
-    summary          varchar(5000) null,
-    published        datetime      null,
-    created          datetime      null,
-    url              varchar(255)  null,
-    image_url        varchar(255)  null,
-    tokenize_content text          null
+    title            varchar(500) charset utf8  null,
+    content          text charset utf8          null,
+    summary          text charset utf8          null,
+    published        datetime                   null,
+    created          datetime                   null,
+    url              varchar(255)               null,
+    image_url        varchar(255)               null,
+    tokenize_content varchar(5000) charset utf8 null
 );
 
 create table post_ratings
 (
     id      bigint auto_increment
         primary key,
-    postId  bigint                   null,
-    rate    int                      null,
-    source  varchar(45) charset utf8 null,
-    created datetime                 null,
+    postId  bigint                    null,
+    rate    int                       null,
+    source  varchar(100) charset utf8 null,
+    created datetime                  null,
     constraint post_ratings_ibfk_1
         foreign key (postId) references posts (id)
 );
@@ -48,32 +41,54 @@ create table post_ratings
 create index postId
     on post_ratings (postId);
 
+create table stocks
+(
+    symbol       varchar(20)               not null
+        primary key,
+    companyName  varchar(255) charset utf8 null,
+    industryCode varchar(20)               null,
+    constraint stocks_ibfk_1
+        foreign key (industryCode) references industries (code)
+);
+
 create table post_tags
 (
-    id        bigint auto_increment
+    id          bigint auto_increment
         primary key,
-    postId    bigint            null,
-    content   text charset utf8 null,
-    symbol    varchar(20)       null,
-    sentiment varchar(3)        null,
+    postId      bigint                    null,
+    symbol      varchar(20)               null,
+    keywordName varchar(255) charset utf8 null,
+    sentiment   varchar(3)                null,
     constraint post_tags_ibfk_1
-        foreign key (postId) references posts (id)
+        foreign key (postId) references posts (id),
+    constraint post_tags_ibfk_2
+        foreign key (symbol) references stocks (symbol)
 );
 
 create index postId
     on post_tags (postId);
 
+create index symbol
+    on post_tags (symbol);
+
 create table stock_keywords
 (
-    symbol    varchar(255) null,
-    keywordId int          null
+    id        int auto_increment
+        primary key,
+    symbol    varchar(20) null,
+    keywordId int         null,
+    constraint stock_keywords_ibfk_1
+        foreign key (symbol) references stocks (symbol),
+    constraint stock_keywords_ibfk_2
+        foreign key (keywordId) references keywords (id)
 );
 
-create table stocks
-(
-    symbol       varchar(20)  not null
-        primary key,
-    companyName  varchar(500) null,
-    industryCode varchar(20)  null
-);
+create index keywordId
+    on stock_keywords (keywordId);
+
+create index symbol
+    on stock_keywords (symbol);
+
+create index industryCode
+    on stocks (industryCode);
 
